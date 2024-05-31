32a9dca30a4933b42d761091b31179ed20e686e6
Sean323 <110384248+Rio-323@users.noreply.github.com>
2024-05-31 19:54:16 +0900

    [Bronze IV] Title: 주사위 세개, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.py
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.py
@@ -1,7 +1,7 @@
 import sys
 a, b, c = map(int, sys.stdin.readline().split())
 
-if a == b and b == c:
+if a == b == c:
     print(10000 + (a * 1000))
 elif a == b or a == c:
     print(1000 + (a * 100))
