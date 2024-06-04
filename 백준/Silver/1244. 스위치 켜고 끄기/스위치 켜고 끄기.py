291620dcb0fdf8571ce35e7afb75708c35d5a9ba
Sean323 <110384248+Rio-323@users.noreply.github.com>
2024-06-04 20:36:24 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.py
@@ -1,8 +1,5 @@
 def change_mode(switches, idx):
-    if switches[idx] == 0:
-        switches[idx] = 1
-    else:
-        switches[idx] = 0
+    switches[idx] ^= 1
 
 n = int(input().strip())
 switches = list(map(int, input().strip().split()))
@@ -11,9 +8,9 @@ students = int(input().strip())
 for _ in range(students):
     gender, num = map(int, input().strip().split())
     if gender == 1:
-        for i in range(num -1, n, num):
+        for i in range(num - 1, n, num):
             change_mode(switches, i)
-    elif gender == 2:
+    else:
         num -= 1
         left = right = num
         while left >= 0 and right < n and switches[left] == switches[right]:
@@ -24,4 +21,4 @@ for _ in range(students):
             right += 1
 
 for i in range(0, n, 20):
-    print(' '.join(map(str, switches[i:i+20])))
+    print(*switches[i:i+20])
