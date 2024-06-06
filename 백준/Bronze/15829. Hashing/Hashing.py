bd5321e49fdbeb9766aa567cf4e703aacba178d7
Sean323 <110384248+Rio-323@users.noreply.github.com>
2024-06-06 17:26:01 +0900

    [Bronze II] Title: Hashing, Time: 44 ms, Memory: 31120 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.py
+++ 백준/Bronze/15829. Hashing/Hashing.py
@@ -4,15 +4,15 @@ input = s.stdin.readline
 def char_to_num(char):
     return ord(char) - 96
 
+r = 31
+M = 1234567891
+
 n = int(input().strip())
-str = input().strip()
-arr = []
-sum = 0
+s = input().strip()
 
-for i in str:
-    arr.append(char_to_num(i))
-
+hash_value = 0
 for i in range(n):
-    sum += arr[i] * (31 ** i)
-    
-print(sum)
+    hash_value += char_to_num(s[i]) * (r ** i)
+    hash_value %= M
+
+print(hash_value)
