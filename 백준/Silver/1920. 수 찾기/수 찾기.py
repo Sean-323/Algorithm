52f39370505a1d887b2387c3629b234c4ecd3d85
Sean323 <110384248+Rio-323@users.noreply.github.com>
2024-06-06 11:04:17 +0900

    [Silver IV] Title: 수 찾기, Time: 180 ms, Memory: 51812 KB -BaekjoonHub

---
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -0,0 +1,10 @@
+n = input()
+sources = list(map(int, input().split()))
+visited = {}
+for source in sources:
+    visited[source] = 1
+
+m = input()
+targets = list(map(int, input().split()))
+for target in targets:
+    print(visited.get(target, 0))
