f91e54ed7e60ba5283db67498b06338ad3a9d4c5
Sean323 <110384248+Rio-323@users.noreply.github.com>
2024-06-06 16:38:34 +0900

    [Bronze I] Title: 전주 듣고 노래 맞히기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.py
+++ 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.py
@@ -1,5 +1,4 @@
 import sys
-
 input = sys.stdin.readline
 
 N, M = map(int, input().strip().split())
@@ -7,28 +6,22 @@ song = {}
 
 for _ in range(N):
     line = input().strip().split()
-    T, name, a1, a2, a3, a4, a5, a6, a7 = line
-    music1 = [a1, a2, a3]
-    song[name] = music1
+    name = line[1]
+    music1 = ' '.join(line[2:5])  # 첫 세 개의 음을 문자열로 저장
+    if music1 not in song:
+        song[music1] = []
+    song[music1].append(name)
 
 results = []
 
 for _ in range(M):
-    music2 = input().strip().split()
-    count = 0
-    title = ""
-
-    for s in song:
-        if music2 == song[s]:
-            count += 1
-            title = s
-
-    if count >= 2:
-        results.append("?")
-    elif count == 1:
-        results.append(title)
+    music2 = ' '.join(input().strip().split())
+    if music2 in song:
+        if len(song[music2]) > 1:
+            results.append("?")
+        else:
+            results.append(song[music2][0])
     else:
         results.append("!")
 
-for result in results:
-    print(result)
+print("\n".join(results))
