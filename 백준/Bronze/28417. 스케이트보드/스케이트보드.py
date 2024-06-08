7750eb812061dd61b188b9720817d019a3ba8cac
Sean323 <110384248+Rio-323@users.noreply.github.com>
2024-06-08 12:08:46 +0900

    [Bronze III] Title: 스케이트보드, Time: 2992 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/28417. 스케이트보드/스케이트보드.py
@@ -0,0 +1,14 @@
+N = int(input())
+result = 0
+
+for _ in range(N):
+    skateboard = list(map(int, input().split()))
+    run = skateboard[:2]
+    trick = skateboard[2:]
+
+    run.sort(reverse=True)
+    trick.sort(reverse=True)
+
+    result = max(result, run[0] + sum(trick[:2]))
+
+print(result)
