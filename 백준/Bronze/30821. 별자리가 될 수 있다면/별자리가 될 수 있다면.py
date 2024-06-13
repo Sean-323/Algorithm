31d7b2add6a664eb5202b0e5a833bd24f4e1ae19
Sean323 <110384248+Rio-323@users.noreply.github.com>
2024-06-13 14:16:05 +0900

    [Bronze III] Title: 별자리가 될 수 있다면, Time: 4344 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/30821. 별자리가 될 수 있다면/별자리가 될 수 있다면.py
+++ 백준/Bronze/30821. 별자리가 될 수 있다면/별자리가 될 수 있다면.py
@@ -1,8 +1,9 @@
-import sys as s
+import sys
 from itertools import combinations
 
-n = int(s.stdin.readline().strip())
-numbers = list(range(1, n + 1))
+input = sys.stdin.read
+n = int(input().strip())
+numbers = range(1, n + 1)
 count = sum(1 for _ in combinations(numbers, 5))
 
 print(count)
