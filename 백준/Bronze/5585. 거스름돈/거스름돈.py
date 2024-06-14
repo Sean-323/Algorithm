1bc2b8fbd64a3dcb7b81481e4aef0c901d1e32fd
Sean323 <110384248+Rio-323@users.noreply.github.com>
2024-06-14 13:18:57 +0900

    [Bronze II] Title: 거스름돈, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/5585. 거스름돈/거스름돈.py
+++ 백준/Bronze/5585. 거스름돈/거스름돈.py
@@ -1,13 +1,12 @@
 import sys as s
 
-n = int(s.stdin.readline().strip())  # 잔돈을 입력 받음
-money = 1000 - n  # 고객에게 거슬러 줄 잔돈
-money_list = [500, 100, 50, 10, 5, 1]  # 사용 가능한 동전 단위
-count = 0  # 총 동전 개수
+n = 1000 - int(s.stdin.readline().strip()) 
+money_list = [500, 100, 50, 10, 5, 1] 
+count = 0  
 
 for i in money_list:
-    if money >= i:  # 잔돈이 현재 동전 단위보다 크거나 같을 때만 처리
-        count += money // i  # 현재 동전 단위로 거슬러 줄 동전 개수 추가
-        money %= i  # 남은 잔돈 업데이트
+    if n >= i: 
+        count += n // i 
+        n %= i 
 
-print(count)  # 총 동전 개수 출력
+print(count)  
