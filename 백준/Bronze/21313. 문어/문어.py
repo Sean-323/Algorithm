278942deadff803809ae9fdb2e8858c45817a4ab
Sean323 <110384248+Rio-323@users.noreply.github.com>
2024-06-17 14:25:56 +0900

    [Bronze II] Title: 문어, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/21313. 문어/문어.py
+++ 백준/Bronze/21313. 문어/문어.py
@@ -1,21 +1,19 @@
-import sys
-n = int(sys.stdin.readline().strip())
+import sys as s
+n = int(s.stdin.readline().strip())
 
-result = []
 
-# 홀수일 경우 마지막 문어가 첫 번째 문어와 손을 잡지 않도록 3을 추가
-if n % 2 == 1:
-    for i in range(1, n + 1):
-        if i % 2 == 0:
-            result.append(2)
-        else:
-            result.append(1)
-    result[-1] = 3  # 마지막 문어는 3번 손으로 변경
-else:
+def octopus(n):
+    result = []
+
     for i in range(1, n + 1):
         if i % 2 == 0:
             result.append(2)
         else:
             result.append(1)
 
-print(' '.join(map(str, result)))
+    if n % 2 == 1:
+        result[-1] = 3
+
+    return ' '.join(map(str, result))
+
+print(octopus(n))
