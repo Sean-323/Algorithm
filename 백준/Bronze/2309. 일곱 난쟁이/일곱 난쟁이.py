1d819975c30f748d6cc90a2af93425e568ecb0fc
Sean323 <110384248+Rio-323@users.noreply.github.com>
2024-10-07 19:38:02 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 32 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -1,11 +1,6 @@
 import itertools
 
 arr = [int(input()) for _ in range(9)]
-comb = list(itertools.combinations(arr, 7))
+result = next(sorted(c) for c in itertools.combinations(arr, 7) if sum(c) == 100)
 
-for c in comb:
-    if sum(c) == 100:
-        result = sorted(c)
-        break
-
-print("\n".join(map(str, result)))
+print("\n".join(map(str,result)))
