04613dcfecd7fb912a07e8fc8d7af5c5f4ddcc0a
Sean323 <110384248+Rio-323@users.noreply.github.com>
2024-10-15 11:12:23 +0900

    [Bronze I] Title: 피보나치 수 2, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
+++ 백준/Bronze/2748. 피보나치 수 2/피보나치 수 2.py
@@ -1,14 +1,8 @@
-def fibonaci(n):
-    numbers = []
-
-    for i in range(n + 1):
-        if i < 2:
-            numbers.append(i)
-        else:
-            numbers.append(numbers[i - 2] + numbers[i - 1])
-
-    return numbers[-1]
+def fibonacci(n):
+    a, b = 0, 1
+    for _ in range(n):
+        a, b = b, a + b
+    return a
 
 n = int(input())
-
-print(fibonaci(n))
+print(fibonacci(n))
