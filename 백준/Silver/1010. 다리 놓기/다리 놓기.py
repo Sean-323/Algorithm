8ac39c7bf98c9f4f35d33bae5667a97bc89023f8
Sean323 <110384248+Rio-323@users.noreply.github.com>
2024-10-17 11:03:54 +0900

    [Silver V] Title: 다리 놓기, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.py
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.py
@@ -1,11 +1,22 @@
-import math
+# 팩토리얼을 계산하는 함수
+def factorial(num):
+    result = 1
+    for i in range(2, num + 1):
+        result *= i
+    return result
 
-t = int(input())
+# 조합을 계산하는 함수
+def combination(m, n):
+    return factorial(m) // (factorial(n) * factorial(m - n))
+
+# 입력 처리
+t = int(input())  # 테스트 케이스 수
 result = []
 
 for _ in range(t):
     n, m = map(int, input().split())
-    result.append(math.comb(m, n))
+    result.append(combination(m, n))  # 조합 계산 결과를 저장
 
+# 결과 출력
 for i in result:
     print(i)
