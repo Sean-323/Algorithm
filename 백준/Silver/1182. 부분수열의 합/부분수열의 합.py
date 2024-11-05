cef1dded30d9cdae2d18ca5166b9923e9f6a29bf
Sean323 <110384248+Rio-323@users.noreply.github.com>
2024-11-05 11:13:25 +0900

    [Silver II] Title: 부분수열의 합, Time: 236 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
+++ 백준/Silver/1182. 부분수열의 합/부분수열의 합.py
@@ -2,11 +2,8 @@ import sys, itertools
 
 N, S = map(int, sys.stdin.readline().split())
 arr = list(map(int, sys.stdin.readline().split()))
-result = 0
 
-for i in range(1, N + 1):
-    for comb in itertools.combinations(arr, i):
-        if sum(comb) == S:
-            result += 1
+# 모든 길이의 조합을 한 번에 체크하며 S와 일치하는 부분합 개수 구하기
+result = sum(1 for i in range(1, N + 1) for comb in itertools.combinations(arr, i) if sum(comb) == S)
 
 print(result)
