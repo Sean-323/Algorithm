2153adfc69a926757a12877c9c588eff85620644
Sean323 <110384248+Sean-323@users.noreply.github.com>
2025-03-30 17:51:13 +0900

    [D4] Title: [S/W 문제해결 기본] 10일차 - Contact, Time: 82 ms, Memory: 25,344 KB -BaekjoonHub

--- SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
+++ SWEA/D4/1238. ［S／W 문제해결 기본］ 10일차 － Contact/［S／W 문제해결 기본］ 10일차 － Contact.java
@@ -1,68 +1,94 @@
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
  
 public class Solution {
-    static int[][] graph;
+    static int N;
+    static List<Integer>[] adj;
     static boolean[] visited;
-    static int maxDepth, maxNum;
  
     public static void main(String[] args) throws IOException {
+ 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
  
-        for (int tc = 1; tc <= 10; tc++) {
-            sb.append("#").append(tc).append(" ");
+        int T = 10;
  
-            // 입력
-            StringTokenizer st = new StringTokenizer(br.readLine().trim());
-            int N = Integer.parseInt(st.nextToken());
+        for (int tc = 1; tc <= T; tc++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+ 
+            N = Integer.parseInt(st.nextToken());
             int start = Integer.parseInt(st.nextToken());
- 
-            graph = new int[101][101];
             visited = new boolean[101];
- 
-            st = new StringTokenizer(br.readLine().trim());
-            for (int i = 0; i < N / 2; i++) {
-                int from = Integer.parseInt(st.nextToken());
-                int to = Integer.parseInt(st.nextToken());
-                graph[from][to] = 1; // 단방향
-            }
- 
-            // 로직
-            bfs(start);
-            sb.append(maxNum).append("\n");
-        }
-        System.out.println(sb);
-    }
- 
-    private static void bfs(int start) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{start, 0}); // 사람 번호, 깊이
-        visited[start] = true;
- 
-        maxDepth = 0;
-        maxNum = 0;
- 
-        while (!q.isEmpty()) {
-            int[] cur = q.poll();
-            int now = cur[0];
-            int depth = cur[1];
- 
-            if (depth > maxDepth) {
-                maxDepth = depth;
-                maxNum = now;
-            } else if (depth == maxDepth) maxNum = Math.max(maxNum, now);
+            adj = new ArrayList[101];
  
             for (int i = 1; i <= 100; i++) {
-                if (graph[now][i] == 1 && !visited[i]) {
-                    visited[i] = true;
-                    q.offer(new int[]{i, depth + 1});
+                adj[i] = new ArrayList<>();
+            }
+ 
+            // 길이 N
+            String[] strArr = br.readLine().trim().split(" ");
+ 
+            // 간선 정보 입력
+            // 페어니까 N/2
+            for (int i = 0; i < N / 2; i++) {
+                int A = Integer.parseInt(strArr[i * 2]);
+                int B = Integer.parseInt(strArr[i * 2 + 1]);
+ 
+                adj[A].add(B);
+            }
+ 
+            int result = bfs(start);
+            System.out.println("#" + tc + " " + result);
+        }
+    }
+ 
+    private static int bfs(int start) {
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        visited[start] = true;
+        List<List<Integer>> nums = new ArrayList<>();
+ 
+        int depth = 0;
+ 
+        while (!q.isEmpty()) {
+            int size = q.size();
+ 
+            List<Integer> currentNums = new ArrayList<>();
+            for (int i = 0; i < size; i++) {
+                int num = q.poll();
+                 
+                currentNums.add(num);
+ 
+                // 꺼낸 정점과 연결된 애들한테 연락하기 위해서
+                // visited 하지 않은 정점에 연락
+                for (int j = 0; j < adj[num].size(); j++) {
+                    int nextNode = adj[num].get(j);
+                    // 이미 연락한 정점이면 넘어가기
+                    if (visited[nextNode])
+                        continue;
+ 
+                    q.add(nextNode);
+                    visited[nextNode] = true;
                 }
             }
+             
+            if(!currentNums.isEmpty()) {
+                nums.add(currentNums);
+                depth++;
+            }
         }
+        List<Integer> last = nums.get(depth-1);
+        Collections.sort(last);
+         
+        return last.get(last.size()-1);
     }
 }
