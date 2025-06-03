17e111ecb560d335a05cabbed949c8cb107e8053
Sean323 <110384248+Sean-323@users.noreply.github.com>
2025-06-03 21:18:15 +0900

    [Silver V] Title: 단어 정렬, Time: 336 ms, Memory: 25916 KB -BaekjoonHub

--- 백준/Silver/1181. 단어 정렬/단어 정렬.java
+++ 백준/Silver/1181. 단어 정렬/단어 정렬.java
@@ -1,40 +1,33 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringBuilder sb = new StringBuilder();
-        
-        // 입력
-        int n = Integer.parseInt(br.readLine());
-        
-        // 중복 제거를 위해 HashSet 사용
-        HashSet<String> wordSet = new HashSet<>();
-        for (int i = 0; i < n; i++) {
-            wordSet.add(br.readLine());
-        }
-        
-        // Set을 List로 변환
-        List<String> words = new ArrayList<>(wordSet);
-        
-        // 길이에 따라 정렬하고, 길이가 같으면 사전순으로 정렬
-        Collections.sort(words, new Comparator<String>() {
+
+        int N = Integer.parseInt(br.readLine().trim());
+
+        Set<String> wordSet = new HashSet<>();
+        for (int i = 0; i < N; i++) wordSet.add(br.readLine().trim());
+
+        String[] words = wordSet.toArray(new String[0]);
+
+        Arrays.sort(words, new Comparator<String>() {
             @Override
             public int compare(String s1, String s2) {
-                if (s1.length() == s2.length()) {
-                    return s1.compareTo(s2); // 사전순 정렬
-                }
-                return s1.length() - s2.length(); // 길이순 정렬
+                if (s1.length() != s2.length()) return s1.length() - s2.length();
+                return s1.compareTo(s2);
             }
         });
-        
+
         for (String word : words) sb.append(word).append("\n");
-        
-        bw.write(sb.toString());
-        bw.flush();
-        bw.close();
-        br.close();
+
+        System.out.println(sb);
     }
 }
