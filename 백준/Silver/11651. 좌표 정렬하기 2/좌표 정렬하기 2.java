0bb8b59e6978ccb57f5410adb6ee42f94cd5bc45
Sean323 <110384248+Sean-323@users.noreply.github.com>
2025-06-03 21:36:12 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 780 ms, Memory: 58324 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.StringTokenizer;
+
+public class Main {
+    static class Point {
+        int x, y;
+
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return x + " " + y;
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int N = Integer.parseInt(br.readLine().trim());
+        Point[] points = new Point[N];
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine().trim());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            points[i] = new Point(x, y);
+        }
+
+        Arrays.sort(points, new Comparator<Point>() {
+            @Override
+            public int compare(Point p1, Point p2) {
+                if (p1.y != p2.y) return p1.y - p2.y;
+                return p1.x - p2.x;
+            }
+        });
+
+        for (Point p : points) sb.append(p).append("\n");
+
+        System.out.println(sb);
+    }
+}
