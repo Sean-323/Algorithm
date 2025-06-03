bd0794c8809285c69e35dc8d1767244827a82f21
Sean323 <110384248+Sean-323@users.noreply.github.com>
2025-06-03 20:47:03 +0900

    [Silver IV] Title: 덱, Time: 172 ms, Memory: 18704 KB -BaekjoonHub

---
+++ 백준/Silver/10866. 덱/덱.java
@@ -0,0 +1,60 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        Deque<Integer> queue = new ArrayDeque<>();
+
+        StringTokenizer st = new StringTokenizer(br.readLine().trim());
+        int N = Integer.parseInt(st.nextToken());
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine().trim());
+            String command = st.nextToken();
+
+            switch (command) {
+                case "push_front":
+                    int value = Integer.parseInt(st.nextToken());
+                    queue.addFirst(value);
+                    break;
+
+                case "push_back":
+                    value = Integer.parseInt(st.nextToken());
+                    queue.addLast(value);
+                    break;
+
+                case "front":
+                    sb.append(queue.isEmpty() ? -1 : queue.peekFirst()).append("\n");
+                    break;
+
+                case "back":
+                    sb.append(queue.isEmpty() ? -1 : queue.peekLast()).append("\n");
+                    break;
+
+                case "size":
+                    sb.append(queue.size()).append("\n");
+                    break;
+
+                case "empty":
+                    sb.append(queue.isEmpty() ? 1 : 0).append("\n");
+                    break;
+
+                case "pop_front":
+                    sb.append(queue.isEmpty() ? -1 : queue.pollFirst()).append("\n");
+                    break;
+                case "pop_back":
+                    sb.append(queue.isEmpty() ? -1 : queue.pollLast()).append("\n");
+                    break;
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
