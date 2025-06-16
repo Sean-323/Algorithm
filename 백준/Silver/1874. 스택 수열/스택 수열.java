14d6054f6c93d0c664949259813952ebdb62b031
Sean323 <110384248+Sean-323@users.noreply.github.com>
2025-06-16 22:19:07 +0900

    [Silver II] Title: 스택 수열, Time: 300 ms, Memory: 26472 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,38 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(br.readLine()); 
+        int[] sequence = new int[n];
+
+        for (int i = 0; i < n; i++) sequence[i] = Integer.parseInt(br.readLine());
+
+        Stack<Integer> stack = new Stack<>();
+        int current = 1; 
+        boolean possible = true;
+
+        for (int i = 0; i < n; i++) {
+            int target = sequence[i];
+
+            while (current <= target) {
+                stack.push(current);
+                sb.append("+\n");
+                current++;
+            }
+
+            if (stack.peek() == target) {
+                stack.pop();
+                sb.append("-\n");
+            } else {
+                possible = false;
+                break;
+            }
+        }
+
+        System.out.print(possible ? sb : "NO\n");
+    }
+}
