40d1a85fdf0789218ff5641c69a919ee3967a3b6
Sean323 <110384248+Sean-323@users.noreply.github.com>
2025-07-06 19:23:44 +0900

    [Bronze I] Title: 평균, Time: 108 ms, Memory: 14356 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,26 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-
-        int N = Integer.parseInt(br.readLine().trim());
-        double[] scores = new double[N];
-
-        st = new StringTokenizer(br.readLine().trim());
-        for (int i = 0; i < N; i++) scores[i] = Double.parseDouble(st.nextToken());
-        Arrays.sort(scores);
-
-        double result = 0;
-
-        for (int i = 0; i < N; i++) result += ((scores[i] / scores[N - 1]) * 100);
-
-        System.out.println(result / N);
+        
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        double max = 0, sum = 0;
+        
+        for (int i = 0; i < N; i++) {
+            double score = Double.parseDouble(st.nextToken());
+            if (score > max) max = score;
+            sum += score;
+        }
+        
+        System.out.println(sum / max * 100 / N);
     }
 }
