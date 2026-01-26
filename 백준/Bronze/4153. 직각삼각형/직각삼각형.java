14b9562cd1d42c96fb002e580fb334ee5b1944b1
Sean323 <110384248+Sean-323@users.noreply.github.com>
2026-01-26 14:51:25 +0900

    [Bronze III] Title: 직각삼각형, Time: 172 ms, Memory: 17640 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.java
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -1,28 +1,17 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] triangle = new int[3];
-
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        StringBuilder sb = new StringBuilder();
         while (true) {
-            StringTokenizer st = new StringTokenizer(br.readLine().trim());
-            for (int i = 0; i < 3; i++)
-                triangle[i] = Integer.parseInt(st.nextToken());
-
-            // 종료 조건
-            if (triangle[0] == 0 && triangle[1] == 0 && triangle[2] == 0)
-                break;
-
-            Arrays.sort(triangle);
-            System.out.println(
-                Math.pow(triangle[2], 2) == Math.pow(triangle[0], 2) + Math.pow(triangle[1], 2)
-                    ? "right" : "wrong");
+            int[] x = {sc.nextInt(), sc.nextInt(), sc.nextInt()};
+            if (x[0]==0 && x[1]==0 && x[2]==0) break;
+            Arrays.sort(x);
+            sb.append(x[0]*x[0] + x[1]*x[1] == x[2]*x[2] ? "right\n" : "wrong\n");
         }
+        System.out.print(sb);
     }
 }
