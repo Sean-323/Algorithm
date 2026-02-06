5b44ce3864013bfcea5f8ac3a8273f23a4dd7053
Sean323 <110384248+Sean-323@users.noreply.github.com>
2026-02-06 15:43:17 +0900

    [level 0] Title: 특이한 정렬, Time: 5.41 ms, Memory: 78.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120880. 특이한 정렬/특이한 정렬.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numlist, int n) {
+        Integer[] arr = Arrays.stream(numlist).boxed().toArray(Integer[]::new);
+
+        Arrays.sort(arr, (a, b) -> {
+            int da = Math.abs(a - n);
+            int db = Math.abs(b - n);
+
+            if (da != db) return da - db;   
+            return b - a;                   
+        });
+
+        return Arrays.stream(arr).mapToInt(Integer::intValue).toArray();
+    }
+}
