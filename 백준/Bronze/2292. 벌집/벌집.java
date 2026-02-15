4bcb1e5dc5563230ec8c02194aa95def5c06f620
Sean323 <110384248+Sean-323@users.noreply.github.com>
2026-02-15 12:51:36 +0900

    [Bronze II] Title: 벌집, Time: 100 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+
+        long layer = 1;   
+        long max = 1;    
+
+        while (N > max) {
+            max += 6 * layer;
+            layer++;
+        }
+
+        System.out.println(layer);
+    }
+}
