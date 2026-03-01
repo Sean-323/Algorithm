9c8e2ece3b1064118f88e7c04f2262a261fbde85
Sean323 <110384248+Sean-323@users.noreply.github.com>
2026-03-01 14:09:24 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 584 ms, Memory: 142160 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -2,55 +2,52 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static List<List<Integer>> graph;
+    static ArrayList<Integer>[] A;
     static boolean[] visited;
-    static int N, M;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st = new StringTokenizer(br.readLine().trim());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-        N = Integer.parseInt(st.nextToken()); // 정점 수
-        M = Integer.parseInt(st.nextToken()); // 간선 수
-
-        graph = new ArrayList<>();
-        for (int i = 0; i <= N ; i++) graph.add(new ArrayList<>());
-
-        for (int i = 0; i < M; i++) {
-            st = new StringTokenizer(br.readLine().trim());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-
-            // 무방향 그래프이므로 양쪽 추가
-            graph.get(u).add(v);
-            graph.get(v).add(u);
+        // 입력
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        
+        A = new ArrayList[n + 1];
+        visited = new boolean[n + 1];
+        for (int i = 1; i <= n; i++) A[i] = new ArrayList<Integer>();
+        
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int s =  Integer.parseInt(st.nextToken());
+            int e = Integer.parseInt(st.nextToken());
+            A[s].add(e);
+            A[e].add(s);
         }
 
-        visited = new boolean[N + 1];
+        // 로직
         int count = 0;
 
-        for (int i = 1; i <= N; i++) {
+        for (int i = 1; i <= n; i++) {
             if (!visited[i]) {
-                dfs(i);
                 count++;
+                DFS(i);
             }
         }
-        
-        sb.append(count).append("\n");
-        System.out.print(sb);
+
+        // 출력
+        System.out.println(count);
     }
-
-    static void dfs(int node) {
-        visited[node] = true;
-
-        for (int next : graph.get(node)) {
-            if (!visited[next]) dfs(next);
+    
+    private static void DFS(int v) {
+        if (visited[v]) return;
+        
+        visited[v] = true;
+        for (int i : A[v]) {
+            if (visited[i] == false) DFS(i);
         }
     }
-
 }
